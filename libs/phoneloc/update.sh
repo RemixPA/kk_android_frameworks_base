119ee495a799a7d9f0fcd85ed19c781596b3942a
xilence2210 <xilence2210@gmail.com>
2014-02-06 02:05:04 +0800

    phonloc: update path
    Note that phonloc bootloop can be solved by art blacklist
    
    Change-Id: Ie2d511069e69543e3df07c8c0fce6e7d1d45576d

--- libs/phoneloc/update.sh
+++ libs/phoneloc/update.sh
@@ -17,7 +17,7 @@
 #
 
 make
-./generateData Mps -c mokee-phoneloc.txt
-cp mokee-phoneloc.dat $dirname $(dirname $(dirname $(dirname $(dirname $(pwd)))))/vendor/mk/prebuilt/common/media/mokee-phoneloc.dat
+./generateData Mps -c phoneloc.txt
+cp phoneloc.dat $dirname $(dirname $(dirname $(dirname $(dirname $(pwd)))))/vendor/pa/prebuilt/etc/phoneloc.dat
 make clean
-rm mokee-phoneloc.dat
+rm phoneloc.dat
